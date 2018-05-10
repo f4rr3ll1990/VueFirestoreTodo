a1012d2b737f49dd73c3bd771da05ce473bef462
F4rr3LL <shadow_2pac@mail.ru>
2018-05-11 00:09:46 +0300

    Initial commit

---
+++ App.vue
@@ -0,0 +1,60 @@
+<template>
+	<div id="app">
+		<div>
+			<input type="text" v-model="newReptile" @keyup.enter="addReptile">
+			<button  @click="addReptile">Add</button>
+		</div>
+		<ul class="reptileList">
+			<li v-for="reptile in reptiles" >
+				{{ reptile.name }} -
+				<button @click="deleteReptile(reptile)">Remove</button>
+			</li>
+		</ul>
+	</div>
+
+</template>
+
+<script>
+import { db } from "./firebase";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      todos: [],
+      newTodo: ""
+    };
+  },
+  firestore() {
+    return {
+      todos: db.collection("todos")
+    };
+  },
+  methods: {
+    addTodo: function() {
+      this.$firestore.todos.add({
+        name: this.newTodo,
+        timestamp: new Date()
+      });
+      this.newTodo = "";
+    },
+    deleteTodo: function(todo) {
+      this.$firestore.todos.doc(todo[".key"]).delete();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.reptileList {
+  list-style: none;
+}
+</style>
